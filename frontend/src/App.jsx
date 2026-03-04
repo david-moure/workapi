8759561ce7e9de081d86814f85e7624fed7085b9
DavidMoure <david.cerqueira@deus.ai>
2026-03-04 18:29:56 +0100

    uso emotion styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { getAllExercises } from "./services/exercise-service.js";
 import "./App.css";
+import styled from "@emotion/styled";
+
+const Header = styled.button`
+  color: hotpink;
+`;
 function DataGrid() {
   const exercises = getAllExercises().then((data) => {
     console.log(data);
     return data;
   });
 
-  return <h1>Workout Exercises</h1>;
+  return <Header id="">HOLA</Header>;
+  //return <h1 css={style}>Workout Exercises</h1>;
 }
 function App() {
   const [count, setCount] = useState(0);
